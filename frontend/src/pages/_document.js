6e423439e698462896c9aaa0f80400e2a5eb65b6
balbuenadutrajf89-art <balbuenadutrajf89@gmail.com>
2025-09-06 15:44:48 -0300

    Update _document.js

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -2,11 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="pt-BR"> {/* Corrige acessibilidade */}
+    <Html lang="pt-BR">
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Plataforma P2P para troca de moedas de forma segura (USD, BRL, PYG, ARS)" />
+        <meta name="description" content="Plataforma P2P para troca de moedas com segurança" />
       </Head>
       <body>
         <Main />
